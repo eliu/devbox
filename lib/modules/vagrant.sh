8798a4b4082ca1ab7425425d9d5319a46faa5857
eliu <eliuhy@163.com>
2023-12-19 20:35:18 +0800

    Fill the missing function comments

--- lib/modules/vagrant.sh
+++ lib/modules/vagrant.sh
@@ -13,32 +13,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# ----------------------------------------------------------------
 # Execute command as vagrant
+# ----------------------------------------------------------------
 vg::exec() {
   [[ "root" = $(whoami) ]] && su vagrant -c "$@" || $@
 }
 
+# ----------------------------------------------------------------
 # Execute command as root. User vagrant can become root via sudo
+# ----------------------------------------------------------------
 vg::sudo_exec() {
   local context="$MODULE_ROOT/vagrant.sh"
   [[ "root" = $(whoami) ]] && $@ || sudo bash -c ". $context && $@"
 }
 
+# ----------------------------------------------------------------
 # Change owner to vagrant
+# ----------------------------------------------------------------
 vg::chown() {
   vg::sudo_exec "chown -R vagrant:vagrant $1"
 }
 
+# ----------------------------------------------------------------
 # Fix the following error that will cause all running containers stopped unexpectly.
 # ERRO[0000] Refreshing container <containerID>: 
 # error acquiring lock 0 for container <containerID>: file exists
 # ---
 # Issue: https://github.com/containers/podman/issues/16784#issuecomment-1711364992
+# ----------------------------------------------------------------
 vg::enable_linger() {
   vg::sudo_exec "loginctl enable-linger vagrant"
 }
 
+# ----------------------------------------------------------------
 # Append content to vagrant's context
+# ----------------------------------------------------------------
 vg::env() {
   vg::exec "echo \"$@\" >> \$HOME/.bashrc"
 }
