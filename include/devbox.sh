a19cba418b00892647778b14ba818001f86e620e
eliu <eliuhy@163.com>
2024-01-11 21:12:47 +0800

    Fix issue that config loading too late

--- include/devbox.sh
+++ include/devbox.sh
@@ -17,9 +17,9 @@ function require() {
 }
 
 require test config logging
+config::load_from_file
 
 QUIET_FLAG_Q=$(log::is_verbose_enabled || printf -- "-q")
 QUIET_FLAG_S=$(log::is_verbose_enabled || printf -- "-s")
 QUIET_STDOUT=$(log::is_verbose_enabled && echo "/dev/stdout" || echo "/dev/null")
 QUIET_PULL=$(log::is_verbose_enabled || printf -- "--quiet-pull")
-config::load_from_file
